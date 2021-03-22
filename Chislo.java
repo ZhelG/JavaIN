43d8f6df8d23efbfc9a39085062cf6f5367b50db
ZhelG <81005269+ZhelG@users.noreply.github.com>
2021-03-22 20:36:54 +0300

    Add files via upload

---
+++ Chislo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Chislo
+{
+    public static void main(String[] args)
+    {
+        int i = 12;
+        //число 12 в двоичной системе
+        System.out.println(Integer.toBinaryString(i));
+
+    }
+}
